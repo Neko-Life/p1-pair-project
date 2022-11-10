feaf1d0076ff83c0f7a8c66ec5cd84fe43dfd8a7
Neko-Life <nekolife123579@gmail.com>
2022-11-10 15:58:53 +0700

    util: generalize errors

--- helper/util.js
+++ helper/util.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const baseParam = (paramObject = {}) => {
+  if (paramObject?.errors && !Array.isArray(paramObject.errors)) {
+    paramObject.errors = [paramObject.errors].map(el => el.message || el);
+  }
   return {
     user: null,
     errors: [],
