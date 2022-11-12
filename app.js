fabd2d64224694d5c55acd2e34935dc2f0d95016
Neko-Life <nekolife123579@gmail.com>
2022-11-12 09:45:45 +0700

    refactor config system

--- app.js
+++ app.js
@@ -1,25 +1,95 @@
 "use strict";
 
+require('dotenv').config();
+
+/**
+ * ENV VARS RULES
+ * DATABASE_URL OPTIONAL (use config/config.json instead)
+ * NODE_ENV OPTIONAL (default development)
+ * COOKIE_SECURE OPTIONAL (default false)
+ * PORT OPTIONAL (use config/config.json instead)
+ * PGPORT OPTIONAL (use config/config.json instead)
+ * SESSION_SECRET REQUIRED
+ */
+const requiredEnvVars = ["SESSION_SECRET"];
+for (const key of requiredEnvVars) {
+  if ([null, undefined, ""].includes(process.env[key])) {
+    throw new TypeError(`ENV VARIABLE ${key} must exist!`);
+  }
+}
+ 
 const express = require("express");
 const session = require("express-session");
+const { Pool } = require("pg");
+const { readdirSync } = require("fs");
 
 const router = require("./routes");
-
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
+app.set("trust proxy", 1);
+
+let configExist, config;
+
+try {
+  configExist = readdirSync("./config").includes("config.json");
+}
+catch (err) {
+  console.log(err);
+}
+
+if (configExist) {
+  config = require("./config/config.json")[process.env.NODE_ENV || "development"];
+  console.log("Using config:", config);
+}
+
+const PORT = process.env.PORT || config?.expressPort;
+const DB_PORT = process.env.PGPORT || config?.port || 5432;
+const storeOptions = { createTableIfMissing: true, };
+
+if (process.env.DATABASE_URL?.length) {
+  storeOptions.conString = process.env.DATABASE_URL;
+} else if (configExist) {
+  const {
+    username, password, database, host
+  } = config;
+
+  const poolOptions = {
+    port: DB_PORT,
+    user: username, password, database, host
+  };
+
+  storeOptions.pool = new Pool(poolOptions);
+
+  console.log("Testing store connection with pool:", poolOptions);
+  storeOptions.pool.query("SELECT NOW();", (err, res) => {
+    if (err) {
+      console.error(err);
+      console.error("Failed to connect to database, terminating...");
+      process.exit(1);
+    }
+
+    console.log("Store connected!");
+  });
+} else throw new TypeError("No DATABASE_URL in env and no `config/config.json` file to connect to database!");
+
+console.log("Using store:", storeOptions);
+
 app.use(session({
-  secret: "hallo hallo",
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false,
+    secure: [true, "true", 1].includes(process.env.COOKIE_SECURE),
     sameSite: true,
-  }
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  },
+  store: new (require("connect-pg-simple")(session))(storeOptions)
 }));
 
 app.use("/", router);
 
-app.listen(process.env.NODE_ENV === "production" ? process.env.PORT : 3000);
+app.listen(Number(PORT));
--- reset-db.sh
+++ reset-db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Doesn't work for heroku postgres, run `heroku run node heroku-migrate` instead"
 sequelize db:drop $@
 sequelize db:create $@
 sequelize db:migrate $@
